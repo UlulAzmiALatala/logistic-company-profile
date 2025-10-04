58364db3591d56bd85c7a7395fdb06ddb04e5e68
Ulul Azmi A. Latala <ullulazmia.l@gmail.com>
2025-10-04 15:14:12 +0700

    Feat: Replace Inertia Head with React Helmet for static deploy

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { LazyMotion, domAnimation } from "framer-motion";
-import { HelmetProvider } from "react-helmet-async"; // <-- 1. IMPORT INI
+import { HelmetProvider } from "react-helmet-async";
 import Home from "./Pages/Home";
+
+// CSS INTI ANDA
 import "../css/app.css";
+
+// ==> TAMBAHKAN DUA BARIS INI UNTUK MEMPERBAIKI SLIDER <==
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        {/* 2. BUNGKUS SEMUANYA DENGAN HELMETPROVIDER */}
         <HelmetProvider>
             <LazyMotion features={domAnimation}>
                 <Home />
